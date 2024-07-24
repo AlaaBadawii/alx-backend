dcda5e4349e0f6d2bebe9e7b5014e59839b6275d
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-07-24 07:31:59 +0300

    check 2-lifo

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -3,12 +3,17 @@
 2-lifo_cache module
 """
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
 class LIFOCache(BaseCaching):
     """
     LIFOCache a caching system
     """
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
     def put(self, key, item):
         """
         put item into cache
@@ -16,13 +21,14 @@ class LIFOCache(BaseCaching):
         if key is None or item is None:
             return
 
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS and\
-                key not in self.cache_data:
-            last_key = next(reversed(self.cache_data))
-            self.cache_data.pop(last_key)
-            print(f"DISCARD: {last_key}")
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            if key not in self.cache_data:
+                first_key = list(self.cache_data.keys())[0]
+                self.cache_data.pop(first_key)
+                print(f"DISCARD: {first_key}")
 
         self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=False)
 
     def get(self, key):
         """
