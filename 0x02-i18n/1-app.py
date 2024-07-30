02858b1ae9a324037f61d93c9f96b1fb220d068d
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-07-30 22:15:37 +0300

    check 1-app

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,14 +6,13 @@ app = Flask(__name__)
 babel = Babel(app)
 
 
-class Config():
-    """ Config class """
+class Config:
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app.config.from_object(Config)
-babel.default_locale = 'en'
-babel.default_timezone = 'UTC'
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
