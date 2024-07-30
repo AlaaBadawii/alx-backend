1ba3fea9154bd881fe4e5f4a3a687dfcc7a1f87e
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-07-30 23:24:34 +0300

    check

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-""" Basic Flask app """
+""" doc doc doc """
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
 
-class Config:
-    """ Babel configuration """
+class Config(object):
+    """doc doc doc"""
+
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -14,6 +15,7 @@ class Config:
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
+
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
@@ -22,34 +24,33 @@ users = {
 }
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
-def home():
-    """ Home page """
-    return render_template('4-index.html')
-
-
-@babel.localeselector
-def get_locale() -> str:
-    """ doc doc doc """
-    if request.args.get("locale") in app.config["LANGUAGES"]:
-        return request.args.get("locale")
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
-
-
-def get_user():
-    """Get user"""
-    user_id = request.args.get('login_as')
-    if user_id is not None and user_id in users:
-        return users[user_id]
+def get_user() -> dict:
+    """doc doc doc"""
+    user_id = request.args.get("login_as")
+    if user_id is not None and int(user_id) in users:
+        return users[int(user_id)]
     return None
 
 
 @app.before_request
 def before_request():
-    """Before request"""
-    user = get_user()
-    g.user = user
+    """doc doc doc"""
+    g.user = get_user()
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """doc doc doc"""
+    if request.args.get("locale") in app.config["LANGUAGES"]:
+        return request.args.get("locale")
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/")
+def index() -> str:
+    """doc doc doc"""
+    return render_template("5-index.html")
 
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=5000)
+    app.run(host="0.0.0.0", port="5000")
